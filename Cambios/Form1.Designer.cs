23c6766e5f07c719e4f504447abe6904fd655a3f
Ninive Monteiro <ninive.comercial@gmail.com>
2021-05-19 13:12:19 +0100

    Create NuGet / Http / Json

--- Cambios/Form1.Designer.cs
+++ Cambios/Form1.Designer.cs
@@ -30,126 +30,128 @@ namespace Cambios
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.Lbl_Resultado = new System.Windows.Forms.Label();
-            this.Lbl_Status = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.Txt_Valor = new System.Windows.Forms.TextBox();
-            this.Txt_MoedaDestino = new System.Windows.Forms.TextBox();
-            this.Txt_MoedaOrigem = new System.Windows.Forms.TextBox();
-            this.btn_Converter = new System.Windows.Forms.Button();
+            this.Cb_MoedaOrigem = new System.Windows.Forms.ComboBox();
+            this.Cb_MoedaDestino = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Lb_Resultado = new System.Windows.Forms.Label();
+            this.Lbl_Status = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(31, 43);
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(32, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 19);
+            this.label1.Size = new System.Drawing.Size(54, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Valor:";
             // 
-            // Lbl_Resultado
+            // label2
             // 
-            this.Lbl_Resultado.AutoSize = true;
-            this.Lbl_Resultado.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Lbl_Resultado.Location = new System.Drawing.Point(203, 210);
-            this.Lbl_Resultado.Name = "Lbl_Resultado";
-            this.Lbl_Resultado.Size = new System.Drawing.Size(386, 19);
-            this.Lbl_Resultado.TabIndex = 1;
-            this.Lbl_Resultado.Text = "Escolha um valor, moeda de origem e destino:";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(32, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Moeda de Origem:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(32, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Moeda de Destino:";
+            // 
+            // Txt_Valor
+            // 
+            this.Txt_Valor.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Txt_Valor.Location = new System.Drawing.Point(192, 39);
+            this.Txt_Valor.Name = "Txt_Valor";
+            this.Txt_Valor.Size = new System.Drawing.Size(227, 25);
+            this.Txt_Valor.TabIndex = 3;
+            // 
+            // Cb_MoedaOrigem
+            // 
+            this.Cb_MoedaOrigem.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Cb_MoedaOrigem.FormattingEnabled = true;
+            this.Cb_MoedaOrigem.Location = new System.Drawing.Point(192, 99);
+            this.Cb_MoedaOrigem.Name = "Cb_MoedaOrigem";
+            this.Cb_MoedaOrigem.Size = new System.Drawing.Size(227, 26);
+            this.Cb_MoedaOrigem.TabIndex = 4;
+            // 
+            // Cb_MoedaDestino
+            // 
+            this.Cb_MoedaDestino.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Cb_MoedaDestino.FormattingEnabled = true;
+            this.Cb_MoedaDestino.Location = new System.Drawing.Point(192, 153);
+            this.Cb_MoedaDestino.Name = "Cb_MoedaDestino";
+            this.Cb_MoedaDestino.Size = new System.Drawing.Size(227, 26);
+            this.Cb_MoedaDestino.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(483, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Converter";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Lb_Resultado
+            // 
+            this.Lb_Resultado.AutoSize = true;
+            this.Lb_Resultado.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Lb_Resultado.Location = new System.Drawing.Point(123, 228);
+            this.Lb_Resultado.Name = "Lb_Resultado";
+            this.Lb_Resultado.Size = new System.Drawing.Size(362, 18);
+            this.Lb_Resultado.TabIndex = 7;
+            this.Lb_Resultado.Text = "Escolha um valor, moeda de origem e destino";
             // 
             // Lbl_Status
             // 
             this.Lbl_Status.AutoSize = true;
-            this.Lbl_Status.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Lbl_Status.Location = new System.Drawing.Point(21, 340);
+            this.Lbl_Status.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Lbl_Status.Location = new System.Drawing.Point(30, 310);
             this.Lbl_Status.Name = "Lbl_Status";
-            this.Lbl_Status.Size = new System.Drawing.Size(67, 19);
-            this.Lbl_Status.TabIndex = 2;
-            this.Lbl_Status.Text = "Status:";
-            this.Lbl_Status.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(31, 147);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(161, 19);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Moeda de Destino:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(31, 90);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(158, 19);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Moeda de Origem:";
-            // 
-            // Txt_Valor
-            // 
-            this.Txt_Valor.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Txt_Valor.Location = new System.Drawing.Point(198, 38);
-            this.Txt_Valor.Name = "Txt_Valor";
-            this.Txt_Valor.Size = new System.Drawing.Size(396, 26);
-            this.Txt_Valor.TabIndex = 5;
-            // 
-            // Txt_MoedaDestino
-            // 
-            this.Txt_MoedaDestino.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Txt_MoedaDestino.Location = new System.Drawing.Point(198, 140);
-            this.Txt_MoedaDestino.Name = "Txt_MoedaDestino";
-            this.Txt_MoedaDestino.Size = new System.Drawing.Size(396, 26);
-            this.Txt_MoedaDestino.TabIndex = 6;
-            // 
-            // Txt_MoedaOrigem
-            // 
-            this.Txt_MoedaOrigem.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Txt_MoedaOrigem.Location = new System.Drawing.Point(198, 87);
-            this.Txt_MoedaOrigem.Name = "Txt_MoedaOrigem";
-            this.Txt_MoedaOrigem.Size = new System.Drawing.Size(396, 26);
-            this.Txt_MoedaOrigem.TabIndex = 7;
-            // 
-            // btn_Converter
-            // 
-            this.btn_Converter.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btn_Converter.Location = new System.Drawing.Point(486, 265);
-            this.btn_Converter.Name = "btn_Converter";
-            this.btn_Converter.Size = new System.Drawing.Size(108, 46);
-            this.btn_Converter.TabIndex = 8;
-            this.btn_Converter.Text = "Converter";
-            this.btn_Converter.UseVisualStyleBackColor = true;
+            this.Lbl_Status.Size = new System.Drawing.Size(59, 18);
+            this.Lbl_Status.TabIndex = 8;
+            this.Lbl_Status.Text = "Status";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(463, 340);
+            this.progressBar1.Location = new System.Drawing.Point(413, 310);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(182, 23);
+            this.progressBar1.Size = new System.Drawing.Size(173, 23);
             this.progressBar1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(657, 377);
+            this.ClientSize = new System.Drawing.Size(623, 352);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.btn_Converter);
-            this.Controls.Add(this.Txt_MoedaOrigem);
-            this.Controls.Add(this.Txt_MoedaDestino);
-            this.Controls.Add(this.Txt_Valor);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.Lbl_Status);
-            this.Controls.Add(this.Lbl_Resultado);
+            this.Controls.Add(this.Lb_Resultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Cb_MoedaDestino);
+            this.Controls.Add(this.Cb_MoedaOrigem);
+            this.Controls.Add(this.Txt_Valor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Câmbios";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,14 +160,14 @@ namespace Cambios
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label Lbl_Resultado;
-        private System.Windows.Forms.Label Lbl_Status;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox Txt_Valor;
-        private System.Windows.Forms.TextBox Txt_MoedaDestino;
-        private System.Windows.Forms.TextBox Txt_MoedaOrigem;
-        private System.Windows.Forms.Button btn_Converter;
+        private System.Windows.Forms.ComboBox Cb_MoedaOrigem;
+        private System.Windows.Forms.ComboBox Cb_MoedaDestino;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Lb_Resultado;
+        private System.Windows.Forms.Label Lbl_Status;
         private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,23 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿
 namespace Cambios
 {
+    using Modelos;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            LoadRates();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private async void LoadRates()
+        {
+            // bool load;
+
+            progressBar1.Value = 0;
+
+            var client = new HttpClient();// Criar Http para fazer ligação externa via Http
+            client.BaseAddress = new Uri("http://cambios.somee.com");// Endereço base da Api
+            var respose = await client.GetAsync("/api/rates"); // Assincrona - Diz onde esta o controlador da API
+
+            var result = await respose.Content.ReadAsStringAsync(); //Carrega os resultados no formado de uma string para dentro do objeto result
+
+
+            if (!respose.IsSuccessStatusCode)//Se algo correr mau
+            {
+                MessageBox.Show(respose.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);//Pega o json e coloca em uma lista que o guarda do tipo Rates
+
+            Cb_MoedaOrigem.DataSource = rates;
+            Cb_MoedaOrigem.DisplayMember = "Name";
+
+            Cb_MoedaDestino.BindingContext = new BindingContext(); //Separar Cbs, diz que a cb destino tem um binding diferente
+
+            Cb_MoedaDestino.DataSource = rates;
+            Cb_MoedaDestino.DisplayMember = "Name";
+
+            progressBar1.Value = 100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
